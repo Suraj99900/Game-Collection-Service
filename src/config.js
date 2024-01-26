9421e51806cc220fd87f886c6312d48f20d3461d
suraj jaiswal <jaiswaljesus384@gmail.com>
2024-01-26 15:07:10 +0530

    added changes

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 module.exports = {
     twilio: {
         accountSid: 'AC1ccfbaf5f87eca2d5d783199d3c3c9be',
-        authToken: 'd0cfa2ce4eb08b5ef6772f0778d5df1b',
+        authToken: '02b5cda22f009cef85188f855b463ef7',
         twilioPhoneNumber: '+19787057732',
     },
     RAZORPAY: {
